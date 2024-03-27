decf1844a6b00c5b19c1e42496c7fab6d896519d
Beracles <zhoujiang45@qq.com>
2024-03-27 14:12:35 +0800

    csrf

--- src/pgsns/settings.py
+++ src/pgsns/settings.py
@@ -86,6 +86,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pgsns.wsgi.application'
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://pgsoft-pgsns.hf.space',
+]
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
